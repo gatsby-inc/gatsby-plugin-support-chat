1864e6bb6174f8f481cc66d8a6fc73f3bc2e3fa4
Dania <o.dania@icloud.com>
2021-05-18 23:32:18 +0200

    api to send message to slack

---
+++ packages/demo/src/api/send-message.ts
@@ -0,0 +1,35 @@
+import { GatsbyFunctionRequest, GatsbyFunctionResponse } from "gatsby"
+import { WebClient } from "@slack/web-api"
+
+let channelID: string = process.env.ChannelID
+let token:string = process.env.SLACK_TOKEN
+
+const web = new WebClient(token);
+
+interface SlackRequest {
+    channel: string
+    text: string
+}
+
+export default async function handler(
+  req: GatsbyFunctionRequest,
+  res: GatsbyFunctionResponse
+) {
+    if(req.method != "POST" ){
+        return res.status(401).json({
+            message: "Use post method"
+        })
+    }
+    try {
+        let requestData:SlackRequest  = {channel: channelID, text: req.body}
+        const result = await web.chat.postMessage(requestData).
+        then(res => {
+            return res.json()
+        })
+        res.json(result)
+    } catch (error){
+        res.status(500).send(error)
+    }
+}
+
+
