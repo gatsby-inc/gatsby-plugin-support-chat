37defeaaa63976220f55227cec9638bffd059bf6
Dania <o.dania@icloud.com>
2021-05-19 18:28:31 +0200

    hook to receive event and store in file cache

--- packages/demo/src/api/receive-slack-hook.ts
+++ packages/demo/src/api/receive-slack-hook.ts
@@ -7,11 +7,11 @@ const cachePath = path.resolve("./cache")
 
 let cache = flatCache.load(cacheName, cachePath);
 
-async function setKey(key:string, value, cacheTime){
+async function setKey(key, value, cacheTime){
   let now = new Date().getTime()
   let expire = cacheTime === 0 ? false : cacheTime * 1000 * 60
   cache.setKey(key, {
-    expire: expire === false ? false : now + this.expire,
+    expire: expire === false ? false : now + expire,
     data: value
   })
 }
@@ -44,8 +44,12 @@ export default async function handler(
 ) {
     const threadTs = req.body.message.thread_ts
     const message = req.body.message.text
-    if(req.body.message.sub_type ===  "message_replied") {
-      await setKey(threadTs, message, 10)
+    if(req.body.subtype ==  'message_replied') {
+      await setKey(threadTs, message, 10).then(() => {
+        save()
+      })
+    }else{
+      console.log("Didn't process if block")
     }
     return res.status(200).end()
 }
