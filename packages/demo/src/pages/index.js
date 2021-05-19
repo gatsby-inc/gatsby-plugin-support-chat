c1960cde6c2311983e5622c029ed2081fd6de3e1
Max Hawkins <1227297+thinkybeast@users.noreply.github.com>
2021-05-18 18:08:31 -0700

    scaffold landing page with theme-ui

--- packages/demo/src/pages/index.js
+++ packages/demo/src/pages/index.js
@@ -1,9 +1,38 @@
-import * as React from 'react'
+/* @jsx jsx */
+import * as React from "react";
 
-export default function Home () {
+import { Heading, Text, Button, jsx, Flex } from "theme-ui";
+
+const mainStyles = { padding: 96 };
+
+const heroStyles = {
+  display: `flex`,
+  flexDirection: `column`,
+  alignItems: `center`,
+};
+
+const heroHeadingStyles = { fontSize: 7, marginBottom: `1rem` };
+const heroDescriptionStyles = { fontSize: 3, marginBottom: `2rem` };
+
+export default function Home() {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  )
+    <>
+      <main sx={mainStyles}>
+        <section css={heroStyles}>
+          <Heading as="h1" sx={heroHeadingStyles}>
+            Gatsby Support Chat Plugin
+          </Heading>
+          <Text sx={heroDescriptionStyles}>
+            <code>gatsby-plugin-support-chat</code> is a drop-in support chat
+            solution for Gatsby sites, leveraging the power of serverless
+            functions on Gatsby Cloud.
+          </Text>
+          <Flex>
+            <Button mr={4}>Get Started</Button>
+            <Button variant="secondary">Docs</Button>
+          </Flex>
+        </section>
+      </main>
+    </>
+  );
 }
