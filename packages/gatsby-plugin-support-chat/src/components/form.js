3f65f8f04e90c9ec13d74900cd1fe7097b2bce10
jxnblk <jxnblk@gmail.com>
2021-05-20 14:04:36 -0400

    Remove commented code

--- packages/gatsby-plugin-support-chat/src/components/form.js
+++ packages/gatsby-plugin-support-chat/src/components/form.js
@@ -21,45 +21,6 @@ export default function Form({ onSubmit, closeChat, ...props }) {
     if (input.current) input.current.focus()
   }
 
-  // var thread_ts = null
-  /*
-  const sendMessage = text => {
-      var params = {
-        text : text,
-        thread_ts : thread_ts
-      }
-
-      // replace with function api endpoint
-      var url = "http://127.0.0.1:80/test.php";
-      fetch(url, {
-        method: 'post',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(params)
-      }).then(response => {
-          const contentType = response.headers.get("content-type");
-          if (contentType && contentType.indexOf("application/json") !== -1) {
-              return response.json().then(data => {
-                // received json response
-                thread_ts = data.thread_ts;
-                console.log(thread_ts);
-
-                // begin to poll for message updates
-                //setTimeout(pollForUpdates, 3000);
-              });
-          } else {
-              return response.text().then(text => {
-                // ignore non-json response
-              });
-          }
-      });
-
-      return
-  }
-  */
-
   const handleKeyPress = e => {
     switch (e.key) {
       case "Enter":
