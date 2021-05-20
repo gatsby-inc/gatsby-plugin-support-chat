d11a5173e44c508b5b3f5b5d9ae3c226a7edeae3
jxnblk <jxnblk@gmail.com>
2021-05-20 17:05:26 -0400

    Use useEffect hook for setInterval

--- packages/gatsby-plugin-support-chat/src/components/hooks.js
+++ packages/gatsby-plugin-support-chat/src/components/hooks.js
@@ -1,48 +1,48 @@
-import { useState, useRef } from "react"
+import { useState, useEffect, useRef } from "react"
 
 const endpoints = {
   send: "/api/send-message",
   poll: "/api/poll-updates",
 }
 
-const DELAY = 5000
+const DELAY = 1000
 
 export const useSupportChat = () => {
   const [messages, setMessages] = useState([])
   const [thread_ts, setThreadID] = useState(null)
-  let timer
 
   // polling for messages
-  timer = setTimeout(() => {
-    fetch(endpoints.poll, {
-      method: "post",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ thread_ts }),
-    })
-      .then(res => res.json())
-      .then(data => {
-        console.log({ data })
-        if (!data.text) return
-
-        setMessages([
-          ...messages,
-          {
-            user: "bot",
-            text: data.text,
-            time: Date.now(),
-          },
-        ])
+  useEffect(() => {
+    let timer = setInterval(() => {
+      fetch(endpoints.poll, {
+        method: "post",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ thread_ts }),
       })
-  }, DELAY)
+        .then(res => res.json())
+        .then(data => {
+          console.log(data)
+          if (!data.text) return
+
+          setMessages([
+            ...messages,
+            {
+              user: "bot",
+              text: data.text,
+              time: Date.now(),
+            },
+          ])
+        })
+    }, DELAY)
+    return () => {
+      clearInterval(timer)
+    }
+  }, [])
 
   const sendMessage = text => {
-    var params = {
-      text,
-      thread_ts,
-    }
     setMessages([
       ...messages,
       {
@@ -58,7 +58,10 @@ export const useSupportChat = () => {
         Accept: "application/json",
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(params),
+      body: JSON.stringify({
+        message: text,
+        thread_ts,
+      }),
     })
       .then(res => res.json())
       .then(data => {
